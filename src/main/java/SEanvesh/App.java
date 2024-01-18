69a67ebdb7de51794b3507e9d9a66a47beeb5d4d
Anvesh <anweshsunny4002@gmail.com>
2024-01-18 09:53:29 +0530

    mavenjava push

---
+++ src/main/java/SEanvesh/App.java
@@ -0,0 +1,13 @@
+package SEanvesh;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
